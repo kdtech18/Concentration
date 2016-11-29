cf6ba8f5ea75ba47d7559977773f07a57da61f5e
Kenton Duprey <kdtech18@gmail.com>
2016-11-29 12:17:28 -0500

    adding default constructor

--- src/Concentration.java
+++ src/Concentration.java
@@ -7,10 +7,29 @@
 
 
 import javax.swing.*;
+import java.awt.*;
 
-public class Concentration
+public class Concentration extends JFrame
 	{
 		JPanel mainPanel = new JPanel();
+		JPanel center = new JPanel();
+		JPanel east = new JPanel();
+		
+		BorderLayout border = new BorderLayout();
+		GridLayout board = new GridLayout(4, 4, 1, 1);
+		
+	public Concentration()
+	{
+	    super("Concentration");
+	    setSize(450, 450);
+	    setVisible(true);
+	    setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+	    mainPanel.setLayout(border);
+	    
+	    mainPanel.add(center, BorderLayout.CENTER);
+	    mainPanel.add(east, BorderLayout.EAST);
+	    
+	}
 		
 		
 	
