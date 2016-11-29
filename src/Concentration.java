0726a930487a60798735d1be9a84961f81ea70f3
Kenton Duprey <kdtech18@gmail.com>
2016-11-29 12:32:28 -0500

    adding default constructor

--- src/Concentration.java
+++ src/Concentration.java
@@ -8,8 +8,10 @@
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Concentration extends JFrame
+public class Concentration extends JFrame implements ActionListener
 	{
 		JPanel mainPanel = new JPanel();
 		JPanel center = new JPanel();
@@ -17,6 +19,11 @@ public class Concentration extends JFrame
 		
 		BorderLayout border = new BorderLayout();
 		GridLayout board = new GridLayout(4, 4, 1, 1);
+		
+		int numMatches;
+		JButton newGame = new JButton("Start!");
+		JLabel matches = new JLabel("Matches: " + numMatches);
+		JButton[] cards = new JButton[16];
 		
 	public Concentration()
 	{
@@ -29,9 +36,34 @@ public class Concentration extends JFrame
 	    mainPanel.add(center, BorderLayout.CENTER);
 	    mainPanel.add(east, BorderLayout.EAST);
 	    
+	    center.setLayout(board);
+	    east.add(newGame, BorderLayout.NORTH);
+	    east.add(matches, BorderLayout.CENTER);
+	    
+	    addCards();
+	    
+	    add(mainPanel);
+	    validate();
 	}
-		
-		
 	
+	public void addCards()
+		{
+			center.setLayout(board);
+		for (int i = 0; i < cards.length; i++)
+			{
+				center.add(cards[i]);
+				cards[i].addActionListener(this);
+			}
+			
+		}
 	
+	public void actionPerformed(ActionEvent e)
+		{
+			
+		}
+	
+	public static void main(String[] args)
+		{
+			Concentration app = new Concentration();
+		}// end main method
 	}
